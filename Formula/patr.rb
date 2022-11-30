03577d4d40d875489f36a9da26fa3735e09a6b20
drone <kavin.raj@vicara.co>
2022-11-30 15:19:12 +0000

    patr cli updated to version 0.2.5

--- Formula/patr.rb
+++ Formula/patr.rb
@@ -1,8 +1,8 @@
 class Patr < Formula
 	desc "CLI for interacting with PATR resources"
 	homepage "https://github.com/kavin-kr/homebrew-patr" #todo
-	url "https://github.com/kavin-kr/test-release/releases/download/v0.2.4/patr-0.2.4-dawrin-amd64.tar.gz" #todo
-	sha256 "a3f85f14d07837a8225ffb55bd38846c7ee92bb197b77ec50c46b924ac3afc90"
+	url "https://github.com/kavin-kr/test-release/releases/download/v0.2.5/patr-0.2.5-darwin-amd64.tar.gz" #todo
+	sha256 "616f15aac24159a3bc40f395256c9ac56760f3a364bc2f52eed8c326d3efb02d"
 
 	def install
 		bin.install 'patr'
